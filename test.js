eca9cb555437e525d5066c9bcd07ec88f259e760
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-23 12:56:16 +0900

    Tuesday January 17th, 2023
    Tuesday January 17th, 2023

--- test.js
+++ test.js
@@ -1,10 +1,2 @@
-let words = "The rain in Spain falls mainly on the plain.";
+//Tuesday January 17th, 2023
 
-let result5 = "The " + words.substring(4, 8).toUpperCase() + " " +
-              words.substring(4, 8).toUpperCase() + " " +
-              words.substring(4, 8).toUpperCase() + " on the " +
-              words.substring(38, 43).toUpperCase() + " " +
-              words.substring(38, 43).toUpperCase() + " " +
-              words.substring(38, 43).toUpperCase() + ".";
-
-console.log(result5); // Output: "The RAIN RAIN RAIN on the PLAIN PLAIN PLAIN."
