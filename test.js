c5cc8b4db4d3251795270711b7cad82c9a7b32f1
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-02-27 10:24:59 +0900

    Done
    Done

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 // January 24th
 
 
+// let counter = 1;
+// while (counter <= 10 ) {
+//     console.log(counter);
+//     counter += 1;
+// };
+
+for (let counter = 1; counter <= 10; counter += 1) {
+    console.log(counter);
+  }
