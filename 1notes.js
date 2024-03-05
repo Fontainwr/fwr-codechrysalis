42e84c01a9f252520c79acbe2b98684eb13155d5
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-03-05 15:53:28 +0900

    done
    done

--- 1notes.js
+++ 1notes.js
@@ -2063,3 +2063,38 @@ window.addEventListener("load", () => {
 });
 
 
+
+
+
+
+
+//Example of using DOM of creating a buton clicker with event listener
+//with using arrow up/down key function. 
+
+let counterClick = 0; 
+
+function clickHandle() {
+        counterClick++
+        updateButtonText();
+};
+
+function updateButtonText() {
+    document.getElementById("push").innerText = counterClick;
+}
+
+function handleArrow(event) {
+    if( event.key === "ArrowUp") {
+        counterClick++;
+        updateButtonText();
+    } else if (event.key === "ArrowDown"){
+        counterClick--;
+        updateButtonText();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("push").addEventListener("click", clickHandle);
+    document.addEventListener("keydown", handleArrow);
+})
+
+
--- code.js
+++ code.js
@@ -1,2 +1,29 @@
-let counter = 0; 
+let counterClick = 0; 
 
+
+
+function clickHandle() {
+        counterClick++
+        updateButtonText();
+};
+
+
+function updateButtonText() {
+    document.getElementById("push").innerText = counterClick;
+}
+
+
+function handleArrow(event) {
+    if( event.key === "ArrowUp") {
+        counterClick++;
+        updateButtonText();
+    } else if (event.key === "ArrowDown"){
+        counterClick--;
+        updateButtonText();
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("push").addEventListener("click", clickHandle);
+    document.addEventListener("keydown", handleArrow);
+})
