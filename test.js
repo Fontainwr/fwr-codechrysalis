865ae03da0be9eaad96d4dc3a6dea3852e99160b
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-03-20 21:10:26 +0900

    done
    done

--- test.js
+++ test.js
@@ -1,24 +1,25 @@
-/////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 // [ .map() ] = Creates a new array
 // [ .filter() ] = Create new array and checks 
-// [ .push()] = push into main item. 
-// [ .slice(x,y)] = get characters or range 
-// [ .split("")] = organize via space 
+// [ .push() ] = push into main item. 
+// [ .slice(x,y) ] = get characters or range 
+// [ .split("") ] = organize via space 
 // [ .includes() ] = check if includes item : for numbers .toString().includes("X")
-// [ .startsWith() ] = begins with 
-// [ .repeat()] = Repeats 
-// [ .reduce()] = Iterate over each number in array for sum -> e((A,B) => {..}, 0);
-// [ .push()] = Into a new box 
+// [ .startsWith() ] = Cehcks what begins with 
+// [ .repeat() ] = Returns a new string which contains the specified item (X) amount of times 
+// [ .reduce() ] = Iterate over each number in array for sum -> e((A,B) => {..}, 0);
+// [ .push() ] = Into a new box 
 // [ .charAt() ] = Select the character at a position.  
+// [ Math.sign() ] = Check weather a number is positive or zero -> Math.sign(number) === 1,
+// [ Math.floor(Math.random()) ] = random number 
+//============================================================================================
 // [ switch arr spots ] =   [X[0], X[X.length - 1]] = [X[X.length - 1],X[0]];
 // [ converts ] -> number to a string!!! --> let str = number + "";
-// [ Math.sign()] = Check weather a number is positive or zero -> Math.sign(number) === 1,
-// [ Math.floor(Math.random())] = random number 
 // [ Random number ] = const X = max => Mathflor(MathRan() * max) + 1 = return x(num)
 // [ remove Spaces  ] = return str.split("").filter( space => space !== " ").join("") 
 // [ for (let X of str)] = checking lists in array
-// [ return ] = X.length > 0 ? X : false; ---> condition ? expression1 : expression2
+// [ return conditions  ] = X.length > 0 ? X : false; ---> condition ? expression1 : expression2
 // [ square number ] = number ** 2
-/////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 //#6
