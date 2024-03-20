8ed358f9a5a96a87d9db2f07af7fe82c7463e9bc
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-03-20 11:32:34 +0900

    done
    done

--- test.js
+++ test.js
@@ -2,15 +2,23 @@
 
 // [ .map ] = New item
 // [ .filter ] = check 
-// [ .slice(x,y)] = greg a character  
+// [ .slice(x,y)] = get characters or range 
 // [ .split("")] = organize via space 
-// [ .includes ] = includes item  
-// [startsWith ] = begins with 
+// [ .includes ] = check if includes item  
+// [ startsWith ] = begins with 
 // [ .repeat] = Repeats 
-// convertts number to a string!!! --> let str = number + "";
+// [ .reduce()] = Iterate over each number in array for sum -> e((A,B) => {..}, 0);
+// [ .push()] = Into a new box 
+// [ switch arr spots ] =   [X[0], X[X.length - 1]] = [X[X.length - 1],X[0]];
+// [ converts ] -> number to a string!!! --> let str = number + "";
+// [ Math.sign()] = Check weather a number is positive or zero -> Math.sign(number) === 1,
 // [ Math.floor(Math.random())] = random number 
-//[ empty ] = return str.split("").filter( space => space !== " ").join("") 
+// [ empty ] = return str.split("").filter( space => space !== " ").join("") 
+// [ for (let X of str)] = checking lists in array
+// [ return ] = X.length > 0 ? X : false; ---> condition ? expression1 : expression2
 
 
-//#7 --> #2 
-console.log(onlyPositiveVibes([1, -5, 2, 7, -4, 12])); // returns [1, 2, 7, 12]
+
+
+console.log(anyoneThere([1, 2, 3])); // true
+console.log(anyoneThere([])); // false
