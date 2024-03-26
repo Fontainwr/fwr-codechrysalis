0afe03c43b75a2e5543db3d808cab0c5bfcd8987
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-03-26 14:58:02 +0900

    done
    done

--- test.js
+++ test.js
@@ -121,37 +121,33 @@
 // [ Math.sign() ] = Check weather a number is positive or zero -> Math.sign(number) === 1,
 // [ .push() ] = Into a new box
 // [ .push() ] = push into main item.
+// [ .toString ] =  Converts str into number -->  return num.toString().includes('0');
+// [ parseInt()] =  Convert boolean result back to number ->   return parseInt(num.toString().includes('0'))
 // [ .reduce() ] = Iterate over each number in array for sum -> e((sum,num) => { A + B}, 0);
 // [ .reverse() ] = reverse the array(first -> last/last -> first) = arr.reverse();
 // [ /[a-z].test(X)] = Test regex if true based on info code. 
 // [ .slice(x,y) ] = get characters or range ---> or cur one item = X.slice(2)
 // [ .split("") ] = organize via space....You may need to [ .join("") ] it back
 // [ .startsWith() ] = Cehcks what begins with X
+// [ Typeof ] = Check type of item ---> typeof str.charAt(0) === "string"
 //============================================================================================
-// [ converts ] -> number to a string!!! --> let str = number + "";
 // [ check all String if contains ] = return str.length === 1 && /[a-zA-Z]/.test(str);
 // [ for of loop ] = (let X of str) = checking lists in array
 // [ Get middle item in array ] = return arr[Math.round((arr.length -1) / 2)]
 // [ Loop ] = ( let i = 1; i <= X ; i++ ) // or // ( let i = 1; i <= X.length; i++ )
-// [ Random number ] = const X = max => Mathflor(MathRan() * max) + 1 = return x(num)
+// [ Random Number ] = return Math.floor(Math.random() * num) + 1;
 // [ remove Spaces ] = return str.split("").filter( space => space !== " ").join("")
-// [ return conditions ] = X.length > 0 ? X : false; ---> condition ? expression1 : expression2
-// [ square number ] = number ** 2
+// [ remove Item in arr] = return X.map( A => A !== undefined ? A : null ).filter( Done => Done !== null)
+// [ square number ] = arr.map(num => num ** 2).filter(result => result);
 // [ switch arr spots ] = if(x.len >= X) ---> [X[0], X[X.length - 1]] = [X[X.length - 1],X[0]];
 // [ to retrive character ] = X[1][0] = "Second" character and "First" character.
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 // console.log()
-let isLetter = (str) => {
-    if ( str.length === 1 && typeof str === "string"){
-        return /[a-zA-Z]/.test(str);
-    } else {
-        return false; 
-    }
+
+let removeEmpty = (arr) => {
+    return arr.map( check => check !== undefined ? check : null).filter( result => result !== null )
 }
 
-console.log(isLetter("H"));    // Output: true
-console.log(isLetter("9"));    // Output: false
-console.log(isLetter("A"));    // Output: true
-console.log(isLetter(""));     // Output: false
-console.log(isLetter("AB"));   // Output: false
+console.log(removeOdd([1, 2, 3, 4, 5, 6]));
+// [2, 4, 6]
