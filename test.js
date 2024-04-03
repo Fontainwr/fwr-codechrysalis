568610e2c5236511b6681786e322513cfc2975f2
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-04-03 20:34:49 +0900

    done
    done

--- test.js
+++ test.js
@@ -74,17 +74,21 @@
 // [ .split("") ] = organize via space....You may need to [ .join("") ] it back
 // [ .startsWith("") ] = Cehcks what begins with X
 // [ Typeof ] = Check type of item ---> typeof str.charAt(0) === "string"
+
 //============================================================================================
+
 // [ count number ] = for ( let i = 0; i < arr.length; i++) if ( arr[i] === 2)
 // [ count up ] =  for (let i = 1; i <= arr; i++) ---> count.push(i)
 // [ check all String if contains ] = return str.length === 1 && /[a-zA-Z]/.test(str);
-// [ check last numbr ] = for ( let i = arr.length - 1; i >= 0; i--) if (arr[i] < 0)
+// [ check last -numbr ] = for ( let i = arr.length - 1; i >= 0; i--) if (arr[i] < 0)..return
 // [ convert arr to obj ] = for (let i = 0; i < arr.length; i += 2)..const key = arr[i]..const value = arr[i + 1];..obj[key] = value
 // [ for of loop ] = (let X of str) = checking lists in array
 // [ Get middle item in array ] = return arr[Math.round((arr.length -1) / 2)]
 // [ Get item in arr] = return str.split(" ").filter(word => word.length > 1).map(word => word.charAt(0))[1];
 // [ Loop ] = ( let i = 1; i <= X ; i++ ) // or // ( let i = 1; i <= X.length; i++ )
-// [ return Object ] = Return obj[key]
+// [ Return Object ] = Return obj[key]
+// [ Convert arr -> obj ] =  let obj = {}..for (let i = 0; i < arr.length; i += 2 )..obj[arr[i]] = arr[i + 1]
+// [ reverse count from end ] =  return array.slice(-number).reverse();
 // [ make arr -> obj ] =  let obj = {value: num, ...}; return obj
 // [ Random to letters ] = (let i = 0; i < num; i++)-- result += X[Math.floor(Math.random() * X.length)]
 // [ Random Number ] = return Math.floor(Math.random() * num) + 1;
@@ -95,7 +99,15 @@
 // [ to retrive character ] = X[1][0] = "Second" character and "First" character.
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
-//9-7
 
-console.log(findLastNegative([-1, -2, -3])); // -3
-console.log(findLastNegative([-1, 5, 6, 7])); // -1
+
+let createObject = (arr) => { 
+    let obj = {};
+    for (let i = 0; i < arr.length; i += 2 ){
+        obj[arr[i]] = arr[i + 1]
+    };
+    return obj;
+};
+
+console.log(createObject(["animal", "cat", "age", 19, "color", "orange"]));
+// Creates: { "animal": "cat", "age": 19, "color": "orange" }
