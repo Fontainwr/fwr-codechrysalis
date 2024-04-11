c970434429bcf7ba4bba225891e3ecbd4b099a62
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-04-11 21:50:13 +0900

    done
    done

--- test.js
+++ test.js
@@ -1,18 +1,19 @@
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // [ .abs() ] = Math.abs(num1 - 10) // [ .charAt() ] // [ .concat()] // [ .endsWith() ] // [ .filter() ] 
 // [ .find(X, index) ] = [ arr.find(element => element > 10) ]// [ .floor(Math.random()) ] // [ .forEach()]
 // [ .includes() ] = .toString().includes("X") // [ .map() ] = arr.map(set => set ).filter( num => num > 0)
 // [ Math.abs() ] = Math.abs(num1 - 10) // [ Math.floor(Math.random()) ] = Math.floor(Math.random() * num);
 // [ Math.sign() ] = + or 0 -> Math.sign(number) === 1, // [ Math.round() ] // [ .push() ] = push into main item.
-// [ .toString ] = num.toString().includes('0') // [ parseInt()] = boolean to number -> parseInt(X.toString().includes('0'))
+// [ .toString ] = num.toString().includes('0') // [ parseInt()] = bool to num -> parseInt(X.toString().includes('0'))
 // [ .reduce() ] = sum of array -> number.reduce((sum,num) => { return sum + num}, 0); 
 // [ .reverse() ] = reverse the array(first -> last/last -> first) = arr.reverse(); // [ .repeat()] 
 // [ .slice(x,y) ] // [ .slice(X)] = Remove character // [ .split("") ] = then  .join("") ] it back
 // [ .startsWith("") ] // [ Typeof ] = typeof str.charAt(0) === "string"....includes string -->typeof str.includes("string")
-//============================================================================================
-// [ add arr nums ] = for ( let i = 0; i < arr.length; i++){const num = arr[i];if( numn > 0 && num % 10 !== 5) {sum += num ;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// [ add arr nums ] = for(let i = 0; i < arr.length; i++){const num = arr[i];if( numn > 0 && num % 10 !== 5) {sum += num ;
 // [ Count number ] = for ( let i = 0; i < arr.length; i++) if ( arr[i] === 2)
 // [ Count up/Back count  ] =  return array.slice(0, number) /  return array.slice(arr.length -  num ).reverse()
 // [ count up ] =  let result = []. for (let i = 1; i <= arr; i++) ---> result.push(i)--> return result 
@@ -20,10 +21,10 @@
 // [ check last - Numbr ] = for ( let i = arr.length - 1; i >= 0; i--) if (arr[i] < 0)..return
 // [ for of loop ] = (let X of str) = checking lists in array
 // [ Get middle item in array ] = return arr[Math.round((arr.length -1) / 2)]
-// [ Get whole number ] =  min = Math.ceil(n1); min = Math.floor(n2); return Math.floor(Math.random() * (max - min + 1)) + min
+// [ Get whole num ] =  min = Math.ceil(n1); min = Math.floor(n2); return Math.floor(Math.random() * (max - min + 1)) + min
 // [ Get item in arr] = return str.split(" ").filter(word => word.length > 1).map(word => word.charAt(0))[1];
-// [ Retrun arr -> obj ] = let obj = {}/for(i = 0; i < arr.len; i+=2)/let key = arr[i];/let val = arr[i + 1]; obj[key] = val;
-// [ Return ever-other ] =  str.split("").filter((word, index) => index % 2 === 0).join("")
+// [ Retrn arr -> obj ] = let obj = {}/for(i = 0; i < arr.len; i+=2)/let key = arr[i];/let val = arr[i + 1]; obj[key] = val;
+// [ Retrn ever-other ] =  str.split("").filter((word, index) => index % 2 === 0).join("")
 // [ Retrun specific character ] = return str.split(" ")[0][2]
 // [ reverse count from end ] =  return array.slice(-number).reverse();
 // [ Random letter using num ] = (let i = 0; i < num; i++)-- result += X[Math.floor(Math.random() * X.length)]
@@ -34,8 +35,7 @@
 // [ search str for letters ] = return str.split(" ").map( letter => x.slice(1,3) === "oo").includes(true)
 // [ switch arr spots ] = if(x.len >= X) ---> [X[0], X[X.length - 1]] = [X[X.length - 1],X[0]]; --> return arr
 // [ to retrive character ] =  return str.split(" ")[1][0]; = "Second" character and "First" character.
-///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//file:///C:/Users/Admin/Desktop/Main%20folder/Coding/Coding%20Things/My%20Code%20Chrysalis%209.pdf
-//#7 
+//10 
