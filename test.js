0312cdcb7a23eebe05f30d8e4f6b0b9525091c0e
Fontain Richardson <119344403+Fontainwr@users.noreply.github.com>
2024-07-02 09:42:32 +0900

    done
    done

--- test.js
+++ test.js
@@ -50,10 +50,90 @@
 
 // 4-9
 
-// Math.floor(Math.random() * num)
-
-let easterEggHunt = (num) => {
-    return Math.floor(Math.random() * num) + 1
-}
-console.log(easterEggHunt(4)); // true
-console.log(easterEggHunt(11)); // false
+3/8/24, 3:41 PM My Code Chrysalis
+https://my.codechrysalis.io/view/cc-fnd-52/3809/ht0yaw4w61ng 1/4
+JavaScript Exercise Set #4
+Back to day view
+日本語
+▼
+3/8/24, 3:41 PM My Code Chrysalis
+https://my.codechrysalis.io/view/cc-fnd-52/3809/ht0yaw4w61ng 2/4
+Problem 1 (Warmup)
+Write a function called anyoneThere that takes in an array. If the array contains something,
+return true . If it is empty, return false .
+console.log(anyoneThere([1, 2, 3])); // true
+console.log(anyoneThere([])); // false
+Problem 2 (Warmup)
+Write a function called getThird that takes in an array. Return the third item in the array. If
+there is no third item, return "No item found."
+console.log(getThird(["corn", "tomato", "broccoli"])); // "broccoli"
+console.log(getThird(["corn", "pineapple"])); // "No item found."
+Problem 3 (Warmup)
+Write a function called mediumSizedOnly that takes in a number. If the number is between
+10 and 100, return the number. Otherwise, return false .
+console.log(mediumSizedOnly(5)); // `false`
+console.log(mediumSizedOnly(18)); // `18`
+Problem 4 (Warmup)
+Write a function called togetherAtLast that takes in two strings and returns those strings
+joined together with a space.
+console.log(togetherAtLast("Hello", "world")); // "hello world"
+Problem 5 (Warmup)
+Write a function thisSparksJoy that takes in an array of numbers and returns a new array
+with all the positive numbers from the original array.
+console.log(thisSparksJoy([1, 2, -5, 7, -100])); // [1, 2, 7]
+▼
+3/8/24, 3:41 PM My Code Chrysalis
+https://my.codechrysalis.io/view/cc-fnd-52/3809/ht0yaw4w61ng 3/4
+g( p y([ , , , , ])); [ , , ]
+Problem 6 (Basic)
+Write a function called weighIn that takes in two strings. Return the string that is longer. If
+they are the same, return both strings in an array.
+console.log(weighIn("cat", "meow")); // "meow"
+console.log(weighIn("mouse", "duck")); // "mouse"
+console.log(weighIn("moose", "goose")); // ["moose", "goose"]
+Problem 7 (Basic)
+Write a function easterEggHunt that takes in a number and returns true if the number
+contains a 0.
+console.log(easterEggHunt(100)); // true
+console.log(easterEggHunt(73)); // false
+Problem 8 (Basic)
+Write a function called getRandomInteger that takes in a number and returns a random
+whole number between 1 and that number.
+Problem 9 (Basic)
+Write a function called getRandomIntegerInRange that takes in two numbers and returns a
+random whole number between those two numbers.
+Problem 10 (Basic)
+Write a function called trimDownABit that takes in an array. Return a new array that
+contains every other element from the original array (in other words, it skips every other
+element).
+console.log(trimDownABit([1, 2, 3, 4, 5, 6, 7])); // [1, 3, 5, 7]
+Problem 11 (Intermediate)
+Write a function called whatYear that takes in an array of objects. Each object will have a
+key 'year'. Return an array of all of the years. Harder: The array should contain only unique
+years sorted in order
+▼
+3/8/24, 3:41 PM My Code Chrysalis
+https://my.codechrysalis.io/view/cc-fnd-52/3809/ht0yaw4w61ng 4/4
+years, sorted in order.
+console.log(whatYear([{year: 1982}, {year: 1975}, {year: 1934}]));
+// [1982, 1975, 1934]
+Problem 12 (Intermediate)
+Write a function called properAddress that takes in a string of a name separated by a
+space, and a title prefix (Mr., Dr. Mrs., etc.). Assuming that the last name of the person is the
+last word in the string, return a string of their title plus last name separated by a space.
+console.log(properAddress("Dr.", "Mortimer Bartholomew")); // "Dr. Bartholome
+console.log(propertAddress("Mrs.", "Christine Marie Jenkins")); // "Mrs. Jenk
+Problem 13 (Intermediate)
+Write a function called tellMeQuickly that takes in a string. The string will contain words
+separated by spaces. Return a new string containing only the first letter in each word.
+console.log(tellMeQuickly("Hello, I am a cat")); // "HIaac"
+Problem 14 (Intermediate)
+Write a function called numJoiner Take in an array of arrays. Each inner array will contain
+two numbers. Return an array of those numbers added together.
+console.log(numJoiner([[1, 2], [3, 3], [5, 0]])); // [3, 6, 5]
+Problem 15 (Intermediate)
+Write a function called searchForTruth that takes in an array of values.
+Return the number of times true appears in the array. However, this must include BOTH
+booleans that are true, and strings that contain the word "true" somewhere within their
+text.
+▼
